9439b2cc1a46fbb2ca7255b06f5edde39baf2494
Mourad Aouinat <mourad.eyes@gmail.com>
2020-02-17 19:11:32 +0100

    Add responsive iPad Pro

--- src/components/SearchFilter.jsx
+++ src/components/SearchFilter.jsx
@@ -4,75 +4,77 @@ import RadioButton from "./RadioButton";
 
 const SearchFilter = () => {
   return (
-    <div>
-      <div className="px-4 py-4 border-t border-gray-900 sm:flex">
-        <div className="flex -mx-2 sm:w-1/2 sm:mx-0">
-          <div className=" w-1/2 px-2">
+    <form>
+      <div className="lg:flex">
+        <div className="px-4 py-4 border-t border-gray-900 sm:flex lg:block lg:w-2/3 lg:border-r lg:border-gray-900">
+          <div className="flex -mx-2 sm:w-1/2 sm:mx-0 lg:w-full">
+            <div className="w-1/2 px-2">
+              <label
+                htmlFor="bedrooms"
+                className="block text-gray-500 text-small font-semibold"
+              >
+                Bedrooms
+              </label>
+              <select
+                className="form-select w-full mt-1"
+                name="bedrooms"
+                id="bedrooms"
+              >
+                <option>4</option>
+              </select>
+            </div>
+            <div className="w-1/2 px-2">
+              <label
+                htmlFor="bathrooms"
+                className="block text-gray-500 text-small font-semibold"
+              >
+                Bathrooms
+              </label>
+              <select
+                className="form-select w-full mt-1"
+                name="bathrooms"
+                id="bathrooms"
+              >
+                <option>2</option>
+              </select>
+            </div>
+          </div>
+          <div className="mt-4 sm:w-1/2 sm:mt-0 sm:px-2 lg:w-full lg:mt-4">
             <label
-              htmlFor="bedrooms"
+              htmlFor="price"
               className="block text-gray-500 text-small font-semibold"
             >
-              Bedrooms
+              Price Range
             </label>
-            <select
-              className="form-select w-full mt-1"
-              name="bedrooms"
-              id="bedrooms"
-            >
-              <option>4</option>
-            </select>
-          </div>
-          <div className=" w-1/2 px-2">
-            <label
-              htmlFor="bathrooms"
-              className="block text-gray-500 text-small font-semibold"
-            >
-              Bathrooms
-            </label>
-            <select
-              className="form-select w-full mt-1"
-              name="bathrooms"
-              id="bathrooms"
-            >
-              <option>2</option>
+            <select className="form-select w-full mt-1" name="price" id="price">
+              <option>Up to $2000 /wk</option>
             </select>
           </div>
         </div>
-        <div className="mt-4 sm:w-1/2 sm:mt-0 sm:px-2">
-          <label
-            htmlFor="price"
-            className="block text-gray-500 text-small font-semibold"
-          >
-            Price Range
-          </label>
-          <select className="form-select w-full mt-1" name="price" id="price">
-            <option>Up to $2000 /wk</option>
-          </select>
+        <div className="px-4 py-4 border-t border-gray-900 lg:w-2/3 lg:border-r lg:border-gray-900">
+          <span className="block text-gray-500 text-small font-semibold">
+            Property Type
+          </span>
+          <div className="sm:flex sm:-mx-2 lg:block lg:mx-0">
+            <RadioButton label="House" value="house" />
+            <RadioButton label="Apartment" value="apartement" />
+            <RadioButton label="Loft" value="loft" />
+            <RadioButton label="Townhouse" value="townhouse" />
+          </div>
         </div>
-      </div>
-      <div className="px-4 py-4 border-t border-gray-900">
-        <span className="block text-gray-500 text-small font-semibold">
-          Property Type
-        </span>
-        <div className="sm:flex sm:-mx-2">
-          <RadioButton label="House" value="house" />
-          <RadioButton label="Apartment" value="apartement" />
-          <RadioButton label="Loft" value="loft" />
-          <RadioButton label="Townhouse" value="townhouse" />
-        </div>
-      </div>
-      <div className="px-4 py-4 border-t border-gray-900">
-        <span className="block text-gray-500 text-small font-semibold">
-          Amenities
-        </span>
-        <div className="sm:flex sm:-mx-2 sm:flex-wrap">
-          <Checkbox name="balcony" label="Balcony" />
-          <Checkbox name="airConditioning" label="AC" />
-          <Checkbox name="pool" label="Pool" />
-          <Checkbox name="beach" label="Beach" />
-          <Checkbox name="petFriendly" label="Pet Friendly" />
-          <Checkbox name="kidFriendly" label="Kid Friendly" />
-          <Checkbox name="parking" label="Parking" />
+        <div className="px-4 py-4 border-t border-gray-900 lg:w-auto">
+          <span className="block text-gray-500 text-small font-semibold">
+            Amenities
+          </span>
+          <div className="sm:flex sm:-mx-2 sm:flex-wrap">
+            <Checkbox name="balcony" label="Balcony" />
+            <Checkbox name="airConditioning" label="AC" />
+            <Checkbox name="pool" label="Pool" />
+            <Checkbox name="beach" label="Beach" />
+            <Checkbox name="petFriendly" label="Pet Friendly" />
+            <Checkbox name="kidFriendly" label="Kid Friendly" />
+            <Checkbox name="parking" label="Parking" />
+          </div>
         </div>
       </div>
       <div className="bg-gray-900 px-4 py-4 sm:text-right">
@@ -83,7 +85,7 @@ const SearchFilter = () => {
           Update results
         </button>
       </div>
-    </div>
+    </form>
   );
 };
 
